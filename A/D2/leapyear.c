b0fe49d04dadaa73405dbd279d2e750f3f226960
Ankit Das <ankitdasxisc@gmail.com>
2023-03-25 20:55:06 +0530

    .

--- A/D2/leapyear.c
+++ A/D2/leapyear.c
@@ -6,7 +6,27 @@
 //           2000 - leap year
 //           2024 - leap year
 
-int main()
-{
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        printf("%d is a leap year\n", year);
+    } else {
+        printf("%d is not a leap year\n", year);
+    }
+
     return 0;
 }
+
+/*
+Enter a year: 1700
+1700 is not a leap year
+
+Enter a year: 2024
+2024 is a leap year
+*/
