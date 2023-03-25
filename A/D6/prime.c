d615c794f9a122ef5e11b6277d0c0b6e7cf5bc60
Ankit Das <ankitdasxisc@gmail.com>
2023-03-26 00:18:37 +0530

    PRIME

--- A/D6/prime.c
+++ A/D6/prime.c
@@ -1,1 +1,38 @@
 // Write a program in C to determine whether a number is prime or not.
+
+#include <stdio.h>
+
+int main() {
+   int n;
+   int isPrime = 1;
+
+   printf("Enter a number: ");
+   scanf("%d", &n);
+
+   if (n < 2) {
+      isPrime = 0;
+   } else {
+      for (int i = 2; i <= n/2; i++) {
+         if (n % i == 0) {
+            isPrime = 0;
+            break;
+         }
+      }
+   }
+
+   if (isPrime) {
+      printf("%d is a prime number", n);
+   } else {
+      printf("%d is not a prime number", n);
+   }
+
+   return 0;
+}
+
+/*
+Enter a number: 17
+17 is a prime number
+
+Enter a number: 35
+35 is not a prime number
+*/
