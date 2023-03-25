3e9ff24b8a6747d42d6ec950aa0e2a36fbcf0ac1
Ankit Das <ankitdasxisc@gmail.com>
2023-03-25 20:51:08 +0530

    .

--- A/D2/greatest.c
+++ A/D2/greatest.c
@@ -1,6 +1,26 @@
 // Write a program in C to find the greatest of three numbers using AND operator.
 
-int main()
-{
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && a > c) {
+        printf("%d is the greatest number\n", a);
+    } else if (b > a && b > c) {
+        printf("%d is the greatest number\n", b);
+    } else if (c > a && c > b) {
+        printf("%d is the greatest number\n", c);
+    } else {
+        printf("The numbers are not distinct\n");
+    }
+
     return 0;
 }
+/*
+Enter three numbers: 23 45 6
+45 is the greatest number
+*/
