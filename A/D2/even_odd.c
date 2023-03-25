448b478d5efcdb4091e5a6df3b57d9669efac848
Ankit Das <ankitdasxisc@gmail.com>
2023-03-25 21:50:05 +0530

    Changed

--- A/D2/even_odd.c
+++ A/D2/even_odd.c
@@ -1,47 +1,19 @@
 // Write a program in C to check whether a given number is even or odd.
 #include <stdio.h>
-int main()
-{
-	int n;
-	printf("\nENTER ANY NUMBER BETWEEN 1 TO 10\n");
-	scanf("%d",&n);
-	switch(n)
-	{
-		case 1: 
-			printf("ODD"); 
-			break;
-		case 2: 
-			printf("EVEN");
-		 	break;
-		case 3: 
-			printf("ODD"); 
-			break;
-		case 4: 
-			printf("EVEN"); 
-			break;
-		case 5: 
-			printf("ODD"); 
-			break;
-		case 6: 
-			printf("EVEN"); 
-			break;
-		case 7: 
-			printf("ODD"); 
-			break;
-		case 8: 
-			printf("EVEN"); 
-			break;
-		case 9: 
-			printf("ODD"); 
-			break;
-		case 10: 
-			printf("EVEN"); 
-			break;
-		default: 
-			printf("\nINVALID INPUT\n"); 
-			break;
-	}
-	return 0;
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is even\n", num);
+    } else {
+        printf("%d is odd\n", num);
+    }
+
+    return 0;
 }
 /*
 ENTER ANY NUMBER BETWEEN 1 TO 10
