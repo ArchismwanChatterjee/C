aab0a7e43b3b311e19c145302234c68ae8a6cd5e
Krishnava Ghosh <110279533+Gadzrux@users.noreply.github.com>
2023-03-25 22:04:29 +0530

    Update greatest.c

--- A/D2/greatest.c
+++ A/D2/greatest.c
@@ -8,14 +8,12 @@ int main() {
     printf("Enter three numbers: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a > b && a > c) {
+    if (a >= b && a >= c) {
         printf("%d is the greatest number\n", a);
-    } else if (b > a && b > c) {
+    } else if (b >= a && b >= c) {
         printf("%d is the greatest number\n", b);
-    } else if (c > a && c > b) {
-        printf("%d is the greatest number\n", c);
     } else {
-        printf("The numbers are not distinct\n");
+        printf("%d is the greatest number\n", c);
     }
 
     return 0;
