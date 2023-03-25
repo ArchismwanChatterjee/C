70e86ee64a177501c8bc1115156ca16453817261
Eshan276 <eshandas2002@gmail.com>
2023-03-25 18:48:09 +0530

    even odd

--- A/D3/even_odd.c
+++ A/D3/even_odd.c
@@ -1,2 +1,18 @@
 // Write a program that accepts a number from 1 to 10. Print whether the number is even or
 // odd using switch case conduct.
+#include <stdio.h>
+int main(){
+    int num;
+    printf("ENTER A NUMBER IN BETWEEN 1 TO 10\n");
+    scanf("%d",&num);
+    switch(num%2){
+        case 0: printf("The number %d is even",num);
+        break;
+        case 1: printf("The number %d is odd",num);
+        break;
+    }
+    return 0;
+}
+/*ENTER A NUMBER IN BETWEEN 1 TO 10
+4
+The number 4 is even*/
