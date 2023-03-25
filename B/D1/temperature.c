66376dfd98d8ad1cac9730552f82706756ed8636
Krishnava Ghosh <krishnavaghosh23@gmail.com>
2023-03-25 12:16:54 +0530

    formatting code and creating folders

--- B/D1/temperature.c
+++ B/D1/temperature.c
@@ -1,13 +1,18 @@
-//Convert Farenheit to Celcius
-#include<stdio.h>
+// Convert Farenheit to Celcius
+#include <stdio.h>
 
-int main(){
-float fahrenheit,celcius;
-printf("Enter temp in farenheit\n");
-scanf("%f",&fahrenheit);
-celcius=(5.0/9)*(fahrenheit-32);
-printf("The temperature in celcius is %f °",celcius);
-return 0;
+int main()
+{
+    float fahrenheit, celcius;
+
+    printf("Enter temp in farenheit\n");
+    scanf("%f", &fahrenheit);
+
+    celcius = (5.0 / 9) * (fahrenheit - 32);
+
+    printf("The temperature in celcius is %f °", celcius);
+
+    return 0;
 }
 /*
 Enter temp in farenheit
