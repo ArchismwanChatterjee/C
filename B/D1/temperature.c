ad39756f15e5cdfc94ab4ca8ec76784c4bddcf1e
Archismwan Chatterjee <archismwancmail@gmail.com>
2023-03-25 11:16:32 +0530

    Convert Fahrenheit to Celcius

---
+++ B/D1/temperature.c
@@ -0,0 +1,16 @@
+//Convert Farenheit to Celcius
+#include<stdio.h>
+
+int main(){
+float fahrenheit,celcius;
+printf("Enter temp in farenheit\n");
+scanf("%f",&fahrenheit);
+celcius=(5.0/9)*(fahrenheit-32);
+printf("The temperature in celcius is %f °",celcius);
+return 0;
+}
+/*
+Enter temp in farenheit
+32
+The temperature in celcius is 0.000000
+*/
