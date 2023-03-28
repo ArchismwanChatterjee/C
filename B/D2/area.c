4d9736a3755a236408511fa9d741e0cc268e3dd7
Archismwan Chatterjee <archismwancmail@gmail.com>
2023-03-28 23:59:24 +0530

    updated D2

--- B/D2/area.c
+++ B/D2/area.c
@@ -5,6 +5,15 @@
 
 int main()
 {
-
+    float area, radius; // variables to store area and radius
+    printf("The radius of the circle is \n");
+    scanf("%f", &radius);                                                 // accepting the radius from the user
+    area = 3.14 * radius * radius;                                        // calculating the area of the circle
+    printf("The area of the circle with radius %f  is %f", radius, area); // displaying the area of the circle
     return 0;
 }
+/* 
+The radius of the circle is 
+2
+The area of the circle with radius 2.000000  is 12.560000
+*/
--- B/D2/swap.c
+++ B/D2/swap.c
@@ -8,6 +8,19 @@
 
 int main()
 {
-
+    int a, b, c;
+    printf("The numbers are\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    c = a;
+    a = b;
+    b = c;
+    printf("The numbers after swapping are %d  %d ", a, b);
     return 0;
 }
+/*
+The numbers are
+23
+45
+The numbers after swapping are 45  23
+*/
