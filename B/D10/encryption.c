c2c3e0edd919ccd0f410a49d02b2dead3a490de3
Archismwan Chatterjee <archismwancmail@gmail.com>
2023-06-28 00:53:11 +0530

    Added D10

---
+++ B/D10/encryption.c
@@ -0,0 +1,72 @@
+// WAP to read any string from the user in uppercase and encrypt the string by adding x(<256)
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+int main()
+{
+
+    char ch[100], s[100];
+    int x, j = 0;
+
+    printf("Type your code: \n");
+    scanf("%s", &ch);
+
+    while (s[j]) // converting to uppercase
+    {
+        s[j] = toupper(ch[j]);
+        j++;
+    }
+
+    printf("Original code is \n");
+    for (int i = 0; i < strlen(s); i++)
+    {
+        printf("%c", s[i]);
+    }
+
+    printf("\n Enter any number < 256 \n");
+    scanf("%d", &x);
+
+    printf("\n Encrypted code is \n");
+
+    if (x < 256)
+    {
+        for (int i = 0; i < strlen(s); i++)
+        {
+            int a = fabs((int)s[i] + x);
+
+            if (a > (int)'Z')
+            {
+                int d = (a - (int)'Z') % 26;
+                if (d != 0)
+                    s[i] = (char)(64 + d);
+                else
+                    s[i] = 'Z';
+            }
+        }
+
+        for (int i = 0; i < strlen(s); i++)
+        {
+            printf("%c", s[i]);
+        }
+    }
+    else
+        printf("Wrong input");
+    return 0;
+}
+
+/*
+OUTPUT:
+
+Type your code:
+ABCD
+Original code is
+ABCD
+Enter any number < 256
+58
+Encrypted code is
+GHIJ
+
+*/
